8493eb2aa30519cf0d69eacfb07ac15510a753a2
shardassaut <sharhabia@gmail.com>
2026-02-04 08:55:25 +0100

    Add basic image bouncing

--- src/main.c
+++ src/main.c
@@ -10,18 +10,16 @@
 typedef struct Logo{
 	Vector2 position;
 	Vector2 speed;
-	Vector2 txt_size;
-	Color color;
-	float size;
 } Logo;
 
 int main ()
 {
-	int frame = 0,txt_width;
-	float tmp, volume;
+	int frame = 0, itr=0, cli =0;
+	float tmp, volume=0.8f;
 	char c = -1;
 	const char *txt = "DVD";
 	bool pause = false;
+	int op[] = {0,1,2,3,4,0,5,2};
 
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
@@ -30,55 +28,57 @@ int main ()
 
 	InitWindow(WIDTH, HEIGTH, "DVD Logo background");
 	SetWindowState(FLAG_WINDOW_RESIZABLE);
-	//SetWindowState(FLAG_BORDERLESS_WINDOWED_MODE);
+	SetWindowState(FLAG_BORDERLESS_WINDOWED_MODE);
 	HideCursor();
 	InitAudioDevice();
 
 	Logo logo = {0};
-	logo.color = (Color){0,0,255,255};
-	logo.size = 125;
+
+	Image img = LoadImage("dvd_lg.png");
+	ImageFormat(&img, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
+	Image cpy = ImageCopy(img);
+	Texture2D txtr = LoadTextureFromImage(img);
 
 	//Font fnt = LoadFont("bbn.ttf");
 	//Font fnt = LoadFontEx("bbn.ttf", logo.size ,NULL, 0);
 	Font fnt = GetFontDefault();
+
 	Sound bop = LoadSound("bop.mp3");
-	SetSoundVolume(bop, 0.8f);
-	logo.speed = (Vector2){2.0f,2.0f};	
 
-	logo.txt_size = MeasureTextEx(fnt,"DVD", logo.size, 10);
-	logo.position = (Vector2){rand()% (int)(WIDTH-logo.txt_size.x*2) + logo.size/2, rand()% (int)(HEIGTH-logo.txt_size.y*2+1) + logo.size/2};
-
-	Vector2 t;
+	SetSoundVolume(bop, volume);
+	logo.speed = (Vector2){2.5f,2.5f};	
+	logo.position = (Vector2){0.0f, 0.0f};
+	logo.position = (Vector2){rand() % (int)(GetScreenWidth() - 3*txtr.width*0.2) +txtr.width*0.2/2, rand()% (int)(GetScreenHeight() - 3*txtr.height*0.2) +txtr.height*0.2/2};
 	
-
+	int blue = 0;
 	// game loop
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
 	{	
 		
 		if(IsKeyPressed(KEY_P)) pause = !pause;
-		
+		if(IsKeyPressed(KEY_UP)){volume -=0.1f; SetSoundVolume(bop, volume+0.1f);}
+		if(IsKeyPressed(KEY_DOWN)){volume +=0.1f; SetSoundVolume(bop, volume-0.1f);}
 		if(pause == false){
-			
 			logo.position.x += logo.speed.x;
 			logo.position.y += logo.speed.y;
-			if(logo.position.x + logo.txt_size.x >= GetScreenWidth() || logo.position.x <= 0){ 
+			if(logo.position.x + txtr.width*0.2 >= GetScreenWidth() || logo.position.x <= 0){ 
 				logo.speed.x *= -1;
 				PlaySound(bop);
 			}
-			if(logo.position.y +logo.txt_size.y*0.7 >= GetScreenHeight() || logo.position.y  <= 0) {
+			if(logo.position.y + txtr.height*0.2 >= GetScreenHeight() || logo.position.y  <= 0) {
 				PlaySound(bop);
 				logo.speed.y *= -1;
 			}
+
+
+			
 		}
 		// drawing
 		BeginDrawing(); 
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
 
-		
-		//DrawRectangle(logo.position.x, logo.position.y, logo.txt_size.x, logo.txt_size.y*0.70, WHITE);
-		t = (Vector2){logo.position.x,logo.position.y - logo.txt_size.y*0.1};
-		DrawTextEx(fnt, txt, t, logo.size, 10.0f, logo.color);
+		DrawTextureEx(txtr, logo.position, 0, 0.2, WHITE);
 		
 		EndDrawing();
 
